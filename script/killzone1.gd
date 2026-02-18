727711ad463cb361be7ab5f8953e1ee101dc37d3
sys-thai69 <cpheav@paragoniu.edu.kh>
2026-02-18 19:32:49 +0700

    add more platform

--- script/killzone1.gd
+++ script/killzone1.gd
@@ -1,18 +1,17 @@
 extends Area2D
 
-
 @onready var timer = $Timer
 
-
-
-
 func _on_body_entered(body: Node2D) -> void:
+    print("Spike hit detected!") # Does this print?
+    print("Body name: ", body.name)
     print("You died1")
     Engine.time_scale = 0.5
-    body.get_node("CollisionShape2D").queue_free()
-    timer.start() # Replace with function body.
+    body.set_physics_process(false)
+    timer.start()
+
 
 
 func _on_timer_timeout() -> void:
     Engine.time_scale = 1
-    get_tree().reload_current_scene() # Replace with function body.
+    get_tree().reload_current_scene()
